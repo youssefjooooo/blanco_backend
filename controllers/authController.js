65ee3d9d14e4629367d6542a4a0d086644b66845
youssefjooooo <youssefm.f45@gmail.com>
2025-04-19 11:45:24 +0200

    add get account data

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const catchAsync = require("../utils/catchAsync");
 const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
 
 const AppError = require("../utils/appError");
 
@@ -36,4 +37,28 @@ exports.login = catchAsync(async (req, res, next) => {
     status: "success",
     token,
   });
+});
+
+exports.getAccountInfo = catchAsync(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
+  }
+  if (!token) return next(new AppError("Please provide a token!", 401));
+  const decoded_token = await promisify(jwt.verify)(
+    token,
+    process.env.JWT_SECRET
+  );
+
+  const user = await User.findById(decoded_token.id);
+  if (!user) return next(new AppError("This user no longer exists!", 404));
+
+  res.status(200).json({
+    status: "success",
+    user,
+  });
+  req.user = user; // optional but useful
 });
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -5,7 +5,10 @@ const authController = require("../controllers/authController");
 const router = express.Router();
 
 router.post("/signup", authController.signup);
+
 router.post("/login", authController.login);
+router.get("/login", authController.getAccountInfo);
 
 router.route("/").get(getUsers);
+
 module.exports = router;
